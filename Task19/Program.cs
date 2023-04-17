dfab4b685a9f5c0561439a73f58c90201868f4d3
Yan <mamul.93@mail.ru>
2023-04-17 20:01:16 +0900

    19.1

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -2,20 +2,20 @@
 
 14212 -> нет
 12821 -> да
-23432 -> да*/ 
+23432 -> да*/
 
 Console.WriteLine("Эта программа принимает на вход пятизначное число: ");
 string number = Console.ReadLine()!;
 int len = number.Length;
 
-if(len == 5)
+if (len == 5)
 {
-   if (number[0] == number[4] && number[1] == number[3])
-   {
-       Console.WriteLine($"{number} - палиндром");
-   }
-   else 
-   {
-       Console.WriteLine($"{number} - не палиндром");
-   }
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не палиндром");
+    }
 }
